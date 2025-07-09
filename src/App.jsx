3c4f75b8df7b11aad3f83528eeed47b89386cfbc
Huzaifa <huzaifawaqar77@gmail.com>
2025-07-09 19:30:33 +0500

    Implemented and Added The Jason Duval Section With the amazing fade out effect of the video section when you scroll through the Jason section and Parallax effect with the 2 right most images

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {ScrollTrigger} from "gsap/all";
 import NavBar from "./sections/NavBar.jsx";
 import Hero from "./sections/Hero.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
+import Jason from "./sections/Jason.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +14,7 @@ export default function App() {
            <NavBar />
             <Hero />
             <FirstVideo />
+            <Jason />
         </main>
     )
 }
